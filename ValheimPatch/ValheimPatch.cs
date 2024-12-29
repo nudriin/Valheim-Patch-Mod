697b74ac598156a4dbd652a51757d9b7e0004068
Nurdin <mrsunnysummer@gmail.com>
2024-12-30 01:53:38 +0700

    feat: disable need roof and fire for workbench and set character move speed to 15f

--- ValheimPatch/ValheimPatch.cs
+++ ValheimPatch/ValheimPatch.cs
@@ -13,5 +13,42 @@ namespace ValheimPatch
         private const string modVersion = "1.0.0";
 
         private readonly Harmony harmony = new Harmony(modGUI);
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        // patch CraftStation class on valheim
+        // and patch in "Start" function
+        [HarmonyPatch(typeof(CraftingStation), "Start")]
+        class Crafting_Patch
+        {
+            //public bool m_craftRequireRoof = true;
+            //public bool m_craftRequireFire = true;
+
+            // reference the original valheim m_craftRequireRoof and m_craftRequireFire properties
+            // to be changed in BepInEx
+            [HarmonyPrefix]
+            static void setCraftingPatch(ref bool __m_craftRequireFire, ref bool __m_craftRequireRoof)
+            {
+                __m_craftRequireFire = false; // change initial value of m_craftRequireFire to false
+                __m_craftRequireRoof = false;
+            }
+        }
+
+        //m_walkSpeed
+        [HarmonyPatch(typeof(Character), "Awake")]
+        class Character_Patch
+        {
+            [HarmonyPrefix]
+            static void setWalkSpeed(ref float __m_walkSpeed)
+            {
+                __m_walkSpeed = 15f; // set walk speed of character
+            }
+        }
+
     }
+
+
 }
