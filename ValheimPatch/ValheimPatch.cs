784fcaa6973dfd07596cd2b900f9309b250ad87f
Nurdin <mrsunnysummer@gmail.com>
2024-12-30 01:34:50 +0700

    feat: setup BepIn and Harmony

--- ValheimPatch/ValheimPatch.cs
+++ ValheimPatch/ValheimPatch.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
 
 namespace ValheimPatch
 {
-    public class ValheimPatch
+    [BepInPlugin(modGUI, modName, modVersion)]
+    [BepInProcess("valheim.exe")]
+    public class ValheimPatch : BaseUnityPlugin
     {
+        private const string modGUI = "Nudriin.ValheimPatch";
+        private const string modName = "Patch by nudriin";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUI);
     }
 }
